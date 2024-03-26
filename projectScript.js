79e14a892f04614cdb6be46ef52389b7141ffd6c
Joshua Furman <jafbird52@gmail.com>
2024-03-26 11:06:27 -0700

    Added selection tool for the gifs and normalized the project aspect ratio to one size of 1000 pixels wide

--- projectScript.js
+++ projectScript.js
@@ -7,3 +7,31 @@ function toggleDescription(documentID) {
     }
 }
 
+function toggleImage(index) {
+    var images = document.querySelectorAll('.image');
+    var buttons = document.querySelectorAll('.buttons button');
+
+    images.forEach(function(image, i) {
+        if (i === index) {
+            image.style.display = 'flex';
+        } else {
+            image.style.display = 'none';
+        }
+    });
+
+    buttons.forEach(function(button, i) {
+        if (i === index) {
+            button.disabled = true;
+        } else {
+            button.disabled = false;
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var images = document.querySelectorAll('.project-images img'); // Select all images
+
+    images.forEach(function(image) {
+        image.style.display = 'none'; // Initially hide all images
+    });
+});
